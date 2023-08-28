83394f552a0e4c4a8720e9354ef1162f9ac5229e
ahmedshams200 <141565868+ahmedshams200@users.noreply.github.com>
2023-08-28 17:02:06 +0300

    Create answers-1.php

---
+++ answers-1.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/* In PHP, a class cannot directly inherit from more than one class, 
+which means it does not support multiple inheritance for classes
+but Instead of having a single class inherit from multiple classes, 
+you can create separate classes and then use composition to include
+ instances of those classes within the main class. */
+  
+
+/*===========================*/
+
+
+
+
+ /*Method overloading refers to the ability to define multiple
+  methods in the same class with the same name but different parameter
+                      ==================
+     Method overriding is the ability of a subclass to provide a specific 
+     implementation for a method that is already defined in its superclass
+
+
+     static and instance methods can be overloaded (have multiple versions with different parameters), 
+     only instance methods can be overridden (provide a new implementation in a subclass).
+  */
